3989815c92c57602835f84613a14708b5d88c4cc
roahn97shah27 <rohan97shah27@gmail.com>
2017-09-10 23:08:41 +0530

    mod4 up

--- script.js
+++ script.js
@@ -21,13 +21,11 @@ Hello Larry
 Hello Paula
 Hello Laura
 Good Bye Jim
-
 WARNING!!! WARNING!!!
 The code does NOT currently work! It is YOUR job to make it work
 as described in the requirements and the steps in order to complete this
 assignment.
 WARNING!!! WARNING!!!
-
 */
 
 // STEP 1:
